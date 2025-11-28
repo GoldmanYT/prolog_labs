91c210d21c21685449932f1d8e1c5bdde2f810ad
GoldmanYT <89934420+GoldmanYT@users.noreply.github.com>
2025-11-28 15:38:40 +0300

    lab 7, 8

---
+++ lab7/lab7.lisp
@@ -0,0 +1,9 @@
+(setq family
+    '(
+        (son (Kuznetsov Daniil Andreevich))
+        (mother (Vinogradova Natalia Anatolyevna))
+        (grandfather (Vinogradova Anatoliy Germanovich))
+        (grandmother (Vinogradova Zinaida Nikolaevna))
+        (father (Kuznetsov Andrey Anatolyevich))
+    )
+)
